03443a479a30d3659ba94f4c6c7b1377e058e1f2
Daniel Coifman <coifmandaniel@gmail.com>
2025-05-17 20:34:27 -0700

    feat: Add Russian Baltic Fleet course assets and update scripts

---
+++ update-naval-course.sql
@@ -0,0 +1,4 @@
+-- Update the learning path image for the Russian Baltic Fleet course
+UPDATE learning_paths
+SET image = '/path-images/russian-baltic-fleet.jpg'
+WHERE title = 'Around the World in 80 Blunders: The Russian Baltic Fleet''s Epic Fail'; 
